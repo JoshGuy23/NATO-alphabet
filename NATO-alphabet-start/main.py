40772452fea41a39f4f1babd0bba76483eed2836
Joshua Hecker <jhecker2001@gmail.com>
2023-11-07 09:08:23 -0600

    Added functionality to catch errors if the user entered non-alphabetic characters.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -6,7 +6,15 @@ nato_dict = {
     row.letter: row.code for (index, row) in nato_csv.iterrows()
 }
 
-user_name = input("Please enter your first name: ").upper()
-nato_name = [nato_dict[letter] for letter in user_name]
+wrong = True
+
+while wrong:
+    try:
+        user_name = input("Please enter your first name: ").upper()
+        nato_name = [nato_dict[letter] for letter in user_name]
+    except KeyError:
+        print("Please only use letters in the English alphabet.")
+    else:
+        wrong = False
 
 print(nato_name)
